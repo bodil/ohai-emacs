bbb8b11c9f23392fdf25b5577cd2e1a98c51dc8f
Bodil Stokke <bodil@bodil.org>
2015-06-06 02:33:04 +0100

    Use Helm if available in sudo-edit.

--- modules/ohai-editing.el
+++ modules/ohai-editing.el
@@ -121,7 +121,10 @@
 (defun sudo-edit (&optional arg)
   (interactive "p")
   (if (or arg (not buffer-file-name))
-      (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
+      (find-file (concat "/sudo:root@localhost:"
+                         (if (fboundp 'helm-read-file-name)
+                             (helm-read-file-name "File: ")
+                           (ido-read-file-name "File: "))))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
 
