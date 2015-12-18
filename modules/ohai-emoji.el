9beff79865cd67de2c1fabecfb01f837a732c2c1
Bodil Stokke <bodil@bodil.org>
2015-12-18 14:08:26 +0000

    A module for displaying emoji using bitmap images.

---
+++ modules/ohai-emoji.el
@@ -0,0 +1,39 @@
+;;; -*- lexical-binding: t -*-
+;;; ohai-emoji.el --- Support graphical emoji when font support is missing.
+
+;; Copyright (C) 2015 Bodil Stokke
+
+;; Author: Bodil Stokke <bodil@bodil.org>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(package-require 'emojify)
+(require 'emojify)
+
+;; Set emojify to only replace Unicode emoji, and do it everywhere.
+(setq emojify-emoji-styles '(unicode)
+      emojify-inhibit-major-modes '())
+
+;; Patch emojify to replace emoji everywhere in programming modes.
+(defun emojify-valid-prog-context-p (beg end) 't)
+
+;; Enable it globally.
+(add-hook 'after-init-hook #'global-emojify-mode)
+
+
+
+(provide 'ohai-emoji)
+;;; ohai-emoji.el ends here
--- ohai/ohai-module-index.el
+++ ohai/ohai-module-index.el
@@ -42,6 +42,7 @@
    (ohai-helm "advanced selection and narrowing" :optional)
    (ohai-eshell "the native Emacs shell" :optional)
    (ohai-unicode "enhanced Unicode support (warning: slows startup)" :optional)
+   (ohai-emoji "display Unicode emoji even if your system doesn't" :optional)
    (ohai-html "HTML, CSS and friends" :optional)
    (ohai-markdown "Markdown support" :optional)
    (ohai-javascript "JavaScript language support" :optional)
