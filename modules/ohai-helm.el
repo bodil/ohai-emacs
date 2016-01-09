d30ff3c7240df089f96072b9c477bb196fe798da
Bodil Stokke <bodil@bodil.org>
2016-01-09 16:59:42 -0600

    Quickly open .emacs.d files using Helm.

--- modules/ohai-helm.el
+++ modules/ohai-helm.el
@@ -63,5 +63,50 @@
 
 (set-face-attribute 'helm-source-header nil :height 0.75)
 
+;; Bind C-c C-e to open a Helm selection of the files in your .emacs.d.
+;; We get the whole list of files and filter it through `git check-ignore'
+;; to get rid of transient files.
+(defun ohai-helm/gitignore (root files success error)
+  (let ((default-directory root))
+    (let ((proc (start-process "gitignore" (generate-new-buffer-name "*gitignore*")
+                               "git" "check-ignore" "--stdin"))
+          (s (lambda (proc event)
+               (if (equal "finished\n" event)
+                   (funcall success
+                            (with-current-buffer (process-buffer proc)
+                              (s-split "\n" (s-trim (buffer-string)))))
+                 (funcall error event))
+               (kill-buffer (process-buffer proc))
+               (delete-process proc))))
+      (set-process-sentinel proc s)
+      (process-send-string proc (concat (s-join "\n" files) "\n"))
+      (process-send-eof proc))))
+
+(defun ohai-helm/files-in-repo (path success error)
+  (let ((files (f-files path nil t)))
+    (ohai-helm/gitignore path files
+                    (lambda (ignored)
+                      (funcall success (-difference files ignored)))
+                    error)))
+
+(defun ohai-helm/find-files-in-emacs-d ()
+  (interactive)
+  (ohai-helm/files-in-repo
+   dotfiles-dir
+   (lambda (files)
+     (let ((relfiles (-filter
+                      (lambda (f) (not (f-descendant-of? f ".git")))
+                      (-map (lambda (f) (f-relative f dotfiles-dir)) files))))
+       (find-file
+        (concat dotfiles-dir
+                (helm :sources (helm-build-sync-source ".emacs.d" :candidates relfiles)
+                      :ff-transformer-show-only-basename helm-ff-transformer-show-only-basename
+                      :buffer "*helm emacs.d*")))))
+   (lambda (err) (warn "ohai-helm/find-files-in-emacs-d: %s" err))))
+
+(global-set-key (kbd "C-c C-e") 'ohai-helm/find-files-in-emacs-d)
+
+
+
 (provide 'ohai-helm)
 ;;; ohai-helm.el ends here
