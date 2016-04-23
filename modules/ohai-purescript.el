08531a98f7e325188faa023f07b67611437090ff
Bodil Stokke <bodil.stokke@tradingtechnologies.com>
2016-04-23 15:52:11 +0100

    Use flycheck-purescript to check PS code.

--- modules/ohai-purescript.el
+++ modules/ohai-purescript.el
@@ -38,33 +38,12 @@
     (ohai/font-lock-replace-symbol 'purescript-mode "\\(=>\\)" "⇒")
     (define-key purescript-mode-map (kbd "→") (lambda () (interactive) (insert "->")))
     (define-key purescript-mode-map (kbd "←") (lambda () (interactive) (insert "<-")))
-    (define-key purescript-mode-map (kbd "⇒") (lambda () (interactive) (insert "=>"))))
-  ;; Define a Flycheck checker for running the PureScript compiler through Pulp.
-  ;; This version comes from https://gist.github.com/cabrera/157699749ea71bae7a16
-  (with-eval-after-load "flycheck"
-    (flycheck-define-checker pulp
-      "Use Pulp to flycheck PureScript code."
-      :commands ("pulp" "--monochrome" "build")
-      :error-patterns
-      ((error line-start
-              (or (and "Error at " (file-name) " line " line ", column " column
-                       (one-or-more not-newline)
-                       (message (one-or-more (not (in "*")))))
+    (define-key purescript-mode-map (kbd "⇒") (lambda () (interactive) (insert "=>")))))
 
-                  (and "psc: " (one-or-more not-newline) "\n"
-                       (message (one-or-more not-newline) "\n")
-                       "at \"" (file-name) "\" (line " line ", column " column ")")
-                  (and "Unable to parse module:\n"
-                       "  \"" (file-name) "\" (line " line ", column " column "):\n"
-                       (message (one-or-more not-newline) "\n"
-                                (one-or-more not-newline) "\n"
-                                (one-or-more not-newline) "\n"))
-                  )
-
-              line-end
-              ))
-      :modes purescript-mode)
-    (add-to-list 'flycheck-checkers 'pulp)))
+(use-package flycheck-purescript
+  :commands flycheck-purescript-setup)
+(with-eval-after-load "flycheck"
+  (add-hook 'purescript-mode-hook 'flycheck-purescript-setup))
 
 ;; A function for generating a likely module name from the current file path.
 ;; We use this in the `ps.module' snippet.
