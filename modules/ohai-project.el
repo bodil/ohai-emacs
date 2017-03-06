84943257efbb6c4e795d4bbe42b65fdadb0d1829
Bodil Stokke <bodil@bodil.org>
2017-03-06 14:52:43 +0000

    Use ibuffer and ibuffer-projectile.

--- modules/ohai-project.el
+++ modules/ohai-project.el
@@ -33,4 +33,14 @@
   :bind ("C-c C-f" . projectile-find-file)
   :diminish projectile-mode)
 
+;; Use ibuffer instead of list-buffers (C-x C-b) and sort by project.
+(use-package ibuffer-projectile
+  :bind ("C-x C-b" . ibuffer)
+  :config
+  (add-hook 'ibuffer-hook
+            (lambda ()
+              (ibuffer-projectile-set-filter-groups)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic)))))
+
 (provide 'ohai-project)
