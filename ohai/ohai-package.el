32ca02b6671f77671e48b9c2ceeb0ea03ec1333a
Bodil Stokke <bodil@bodil.org>
2017-03-11 13:25:12 +0000

    Remove Marmalade, again.

--- ohai/ohai-package.el
+++ ohai/ohai-package.el
@@ -37,7 +37,6 @@
 ;; to use MELPA as well.
 (setq package-user-dir (concat dotfiles-dir "elpa"))
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
