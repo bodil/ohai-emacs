fa21c5531a535c05f8e3f91a3eed7890f3beee7d
Bob Hepple <bob.hepple@gmail.com>
2020-03-25 19:37:00 +1000

    Update ohai-ido.el
    Change flet to cl-flet to avoid deprecation warning at startup

--- modules/ohai-ido.el
+++ modules/ohai-ido.el
@@ -66,7 +66,7 @@ Symbols matching the text at point are put first in the completion list."
   (imenu--make-index-alist)
   (let ((name-and-pos '())
         (symbol-names '()))
-    (flet ((addsymbols (symbol-list)
+    (cl-flet ((addsymbols (symbol-list)
                        (when (listp symbol-list)
                          (dolist (symbol symbol-list)
                            (let ((name nil) (position nil))
